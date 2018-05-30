0f0ecf08a36d02311e6cea8c3c328abc6e4eb8e6
Yaoquan <yaoquanyu1990@gmail.com>
2018-05-29 23:40:07 -0700

    integrated with eureka

--- MockServer/src/main/java/com/coders/democrazy/server/ServerApplication.java
+++ MockServer/src/main/java/com/coders/democrazy/server/ServerApplication.java
@@ -2,8 +2,10 @@ package com.coders.democrazy.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ServerApplication {
 	
 	public static void main(String[] args) {
